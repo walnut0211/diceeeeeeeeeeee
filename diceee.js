df4d92fea9d0fa75f1224ed0924d0e71a696bab1
walnut0211 <91255078+walnut0211@users.noreply.github.com>
2021-12-05 00:51:30 +0800

    Add files via upload

---
+++ diceee.js
@@ -0,0 +1,18 @@
+const randOne = Math.floor(Math.random() * 6) + 1;
+const randTwo = Math.floor(Math.random() * 6) + 1;
+
+const diceImgOne = 'images/dice' + randOne + '.png';
+const diceImgTwo = 'images/dice' + randTwo + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src',diceImgOne);
+document.querySelectorAll('img')[1].setAttribute('src',diceImgTwo);
+
+if (randOne > randTwo){
+  document.querySelector('h1').innerHTML = "😜Player 1 WINS";
+}
+else if (randTwo > randOne){
+  document.querySelector('h1').innerHTML = "Player 2 WINS😝";
+}
+else{
+  document.querySelector('h1').innerHTML = "😕It's a TIE😕";
+}
