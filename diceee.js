58ecec743709370c6b3da8d13c4f6e7c1d2bb96d
walnut0211 <91255078+walnut0211@users.noreply.github.com>
2021-12-05 09:33:03 +0800

    Update diceee.js

--- diceee.js
+++ diceee.js
@@ -1,16 +1,16 @@
-const randOne = Math.floor(Math.random() * 6) + 1;
-const randTwo = Math.floor(Math.random() * 6) + 1;
+const randomOne = Math.floor(Math.random() * 6) + 1;
+const randomTwo = Math.floor(Math.random() * 6) + 1;
 
-const diceImgOne = 'images/dice' + randOne + '.png';
-const diceImgTwo = 'images/dice' + randTwo + '.png';
+const diceImgOne = 'images/dice' + randomOne + '.png';
+const diceImgTwo = 'images/dice' + randomTwo + '.png';
 
 document.querySelectorAll('img')[0].setAttribute('src',diceImgOne);
 document.querySelectorAll('img')[1].setAttribute('src',diceImgTwo);
 
-if (randOne > randTwo){
+if (randomOne > randomTwo){
   document.querySelector('h1').innerHTML = "😜Player 1 WINS";
 }
-else if (randTwo > randOne){
+else if (randomTwo > randomOne){
   document.querySelector('h1').innerHTML = "Player 2 WINS😝";
 }
 else{
